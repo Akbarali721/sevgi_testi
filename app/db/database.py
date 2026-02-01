3b5b4e034be4adc332b996047cc30b374f7ff55b
Akbarali721 <akbaralixoliqov721@gmail.com>
2026-02-01 22:10:09 +0500

    Add DATABASE_URL fallback for Render

--- app/db/database.py
+++ app/db/database.py
@@ -1,17 +1,13 @@
-# app/db/database.py
 import os
-from dotenv import load_dotenv
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL .env da topilmadi")
 
-# Postgres uchun
+# 🔥 ENG MUHIM QATOR
+if not DATABASE_URL:
+    DATABASE_URL = "sqlite:///./app.db"
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
@@ -24,9 +20,6 @@ SessionLocal = sessionmaker(
 )
 
 def get_db():
-    """
-    FastAPI Depends uchun.
-    """
     db = SessionLocal()
     try:
         yield db
